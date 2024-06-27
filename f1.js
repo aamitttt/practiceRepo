4d5874113537a2b1aa1a34a67ad69526e44dc8e1
Amit Kumar <amit.kumar@progcap.com>
2024-06-27 16:26:35 +0530

    added feature two in dev

--- f1.js
+++ f1.js
@@ -3,3 +3,4 @@ console.log(a)
 let aa = 55;
 console.log(aa)
 // added feature one in dev from ashwani
+// added feature two in dev from ashutosh
--- f2.js
+++ f2.js
@@ -2,3 +2,4 @@ let b = 5;
 console.log(b)
 let bb = 55;
 console.log(bb)
+//added feature two in dev by ashwani
